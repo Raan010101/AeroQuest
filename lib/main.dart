c267d89bdd032837d88695a6c7ee683f445e6b23
Raan Nyaana <71020725+Raan010101@users.noreply.github.com>
2026-02-27 03:42:43 +0800

    Removed Firebase setup competely now and reset Flutter base with splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'screens/splash_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const AeroQuestApp());
 }
 
@@ -24,4 +16,3 @@ class AeroQuestApp extends StatelessWidget {
     );
   }
 }
-  
